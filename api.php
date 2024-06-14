05f35e8f867f0066ca55114cb289a88d5159777e
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-14 18:38:45 +0200

    Funziona ma ancora non ho ben capito niente,

--- api.php
+++ api.php
@@ -5,30 +5,27 @@ $fileContent = file_get_contents("dati.json");
 
 
 //se ho i dati per aggiungere uno studente...
-if( isset($_POST["compiti"])) {
+if (isset($_POST["compiti"])) {
 
     //converto il json in un array associativo php
     $toDo = json_decode($fileContent, true);
 
     //creo un nuovo studente
-    $newTask = [
-        "compiti" => $_POST["compiti"]
-    ];
+    $newTask = $_POST["compiti"];
 
 
     //pusho lo studente appena creato nel mio array
-    $toDo[] = $newTask;
+    $toDo[0]["compiti"][] = $newTask;
+    // Da chiedere come posso togliere questo 0
 
     //converto tutto l'array in un json
     $fileContent = json_encode($toDo);
 
     //scrivo il json su disco
     file_put_contents("dati.json", $fileContent);
-    
 }
 
 
 header('Content-Type: application/json');
 
 echo $fileContent;
-
